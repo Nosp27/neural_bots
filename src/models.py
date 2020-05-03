65892827ebc9c8dc279916bf80b91e220f99f6cc
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-05-03 14:14:18 +0300

    Sources from separate folder

--- src/models.py
+++ src/models.py
@@ -5,15 +5,15 @@ def models_init():
     models = {
         'k': MessageBot(
             'models/k_model/model_weights.h5',
-            history_df='models/k_model/lstm_in.txt',
+            history_df='neural_sources/kate_lstm_in.txt',
         ),
         'kb': MessageBot(
             'models/kb_model/kate_babe_model_30_RMSprop_full_history.h5',
-            history_df='models/kb_model/kate_babe_lstm_in.txt',
+            history_df='neural_sources/kate_babe_lstm_in.txt',
         ),
         'ls': MessageBot(
             'models/neural_liska/weights_lisa_63_iterations_lr0005_with_dr.h5',
-            history_df='models/neural_liska/liska_lstm_in.txt',
+            history_df='neural_sources/liska_lstm_in.txt',
         ),
     }
 
